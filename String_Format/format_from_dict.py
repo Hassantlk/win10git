de48ecc6882c23d4726aaa168f7faa0f8d861dc5
Hassan.TLK <hassan.tlk.85@gmail.com>
2023-05-24 21:13:22 +0330

    شروع رونوشت مطالب مهم؛

---
+++ String_Format/format_from_dict.py
@@ -0,0 +1,10 @@
+introduction = 'My name is {first_name} {middle_name} {last_name} AKA the {aka}.'
+full_name = {
+    'first_name': 'Tony',
+    'middle_name': 'Howard',
+    'last_name': 'Stark',
+    'aka': 'Iron Man',
+}
+ 
+# Notice the use of "**" operator to unpack the values.
+print(introduction.format(**full_name))
